ffefb3a201b6599e36a018a02e05e6ccc7443b4f
Rob Reed <reedsmg@gmail.com>
2020-03-04 17:29:41 -0600

    changed host in  deploy.js file

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.87.161.249',
+      host: '3.85.231.246',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
